683cdae0a68738e0779a318ca3ebb4495bb29dc4
Sanjeev Krishna S <sanjeevkrish5002@gmail.com>
2025-12-12 12:28:43 +0530

    Third commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: " https://SanjeevKrishna2470.github.io/TheBlog",
+  base: "/TheBlog/"
+});
