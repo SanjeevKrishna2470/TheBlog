7df8781fc022fe86c9a352344107b0c706bfaf9b
Sanjeev Krishna S <sanjeevkrish5002@gmail.com>
2025-12-13 12:43:52 +0530

    Updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    site: " https://SanjeevKrishna2470.github.io/TheBlog",
-  base: "/TheBlog/"
+   site: " https://SanjeevKrishna2470.github.io/TheBlog",
+  base: '/TheBlog',
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: true
+    }
+  }
 });
